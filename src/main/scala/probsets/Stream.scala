6e14a2bb5cd7189776a54aed1398666178977f20
Alex Clemmer <clemmer.alexander@gmail.com>
2012-10-21 21:54:54 -0600

    Add SpaceSaving heavy hitters implementation

--- src/main/scala/probsets/Stream.scala
+++ src/main/scala/probsets/Stream.scala
@@ -8,6 +8,7 @@ package stream
 
 import scala.annotation.tailrec
 
+import scala.collection.mutable.Map
 import scala.util.{ Random => Random }
 
 /** Core interface for probabilistic sets.
@@ -18,7 +19,14 @@ abstract class AssociativeStreamSampler[T] {
   def size (): Int
   def apply (i: Int): T
   def getSampleSet (): Array[T]
+}
+
+abstract class MappingStreamSampler[T] {
+  def add (item: T): MappingStreamSampler[T]
+  def addAll (items: Array[T]): MappingStreamSampler[T]
   def size (): Int
+  def apply (item: T): (T, Int)
+  def getSampleSet (): Map[T, Int]
 }
 
 /** Simple implementation of reservoir sampling.
@@ -66,6 +74,41 @@ AssociativeStreamSampler[T] {
   def size () =
     if (currIdx >=k) k
     else currIdx
+}
+
+class SpaceSavingSampler[T: Manifest] (k: Int) extends MappingStreamSampler[T] {
+  var sample = Map[T, Int]()
+  
+  def add (item: T): SpaceSavingSampler[T] = {
+    if (sample.contains(item))
+      sample += item -> (sample(item) + 1)
+    else if (sample.size < k)
+      sample += item -> 1
+    else {
+      val y = sample.minBy(_._2)._1
+      sample += item -> (sample(y) + 1)
+      sample -= y
+    }
+
+    this
+  }
+  
+  def addAll (items: Array[T]): SpaceSavingSampler[T] = {
+    @tailrec
+    def loop (i: Int): Unit = {
+      if (i >= items.length) Unit
+      else {
+	add(items(i))
+	loop(i+1)
+      }
+    }
+    loop(0)
+    this
+  }
+  
+  def apply (item: T): (T, Int) = (item, sample(item))
+  def getSampleSet (): Map[T, Int] = sample
+  def size (): Int = sample.size
 }
 
 /** Simple TEMPORARY tests used for dev purposes */
