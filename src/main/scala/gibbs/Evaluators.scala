db16e92b6197df61a87a23d6fbe27cfb54c56601
Alex Clemmer <clemmer.alexander@gmail.com>
2012-11-28 23:39:24 -0700

    Add skeleton Evaluator class for the Gibbs samplers

---
+++ src/main/scala/gibbs/Evaluators.scala
@@ -0,0 +1,11 @@
+package gibbs
+
+import scala.collection.mutable.{ HashMap => HashMap }
+
+class Evaluator (val D: Int, val N: Int, val w: Array[String], val d: Array[Int],
+		 z: Array[Int],
+		 val W: Int, val wIdx: HashMap[String,Int],
+		 allAssignedZ: Array[Int], wAssignedZ: Array[Array[Int]],
+		 allAssignedZInD: Array[Array[Int]]) {
+
+}
