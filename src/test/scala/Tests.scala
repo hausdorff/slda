c03c9b884d191d3d3a5cdd33839694fa04cabfe9
Alex Clemmer <clemmer.alexander@gmail.com>
2013-01-29 01:21:30 -0700

    Put testing code into testing files
    Currently ad hoc tests lie in Gibbs.scala; now that we have a testing
    suite, we should put them there instead.

--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -1,6 +1,7 @@
 package tests
 
 import gibbs._
+import wrangle._
 import org.scalatest.FunSuite
 import java.util.{ Arrays => Arrays }
 
@@ -11,6 +12,8 @@ class StatsTests extends FunSuite {
     val arr2 = Array[Double](0.2)
     val arr3 = Array[Double](1, 1, 1, 1)
     val arr4 = Array[Double](1, 1, 1)
+    val arr5 = Array[Double](1, 1)
+    val arr6 = Array[Double](2, 1)
     
     assert(Arrays.equals(Stats.normalize(arr0), Array[Double]()),
 	   "array should be empty")
@@ -24,7 +27,68 @@ class StatsTests extends FunSuite {
     assert(Arrays.equals(Stats.normalize(arr4),
 			 Array(1.0/3.0, 2.0/3.0, 1)),
 	   "elements are equiprobable")
+    assert(Arrays.equals(Stats.normalize(arr5), Array(0.5, 1)),
+	   "elements are equiprobable")
+    assert(Arrays.equals(Stats.normalize(arr6), Array(2.0/3.0, 1)))
+  }
+
+  test("sampleCategorical") {
+    val arr1 = Array[Double](2, 1)
+    val cdf = Stats.normalize(arr1)
+    var one = 0
+    var two = 0.0
+    for (i <- 0 to 10000000) {
+      val res = Stats.sampleCategorical(cdf)
+      if (res == 0) one += 1
+      else two += 1
+    }
+    assert(one/(one+two) < 0.69 && one/(one+two) > 0.64)
   }
 }
 
-class GibbsTest extends FunSuite { }
+class GibbsTest extends FunSuite {
+  test("Gibbs sampler on sim-3") {
+    // Test that the objects gets made n stuff
+    println("LOADING CORPUS...")
+    println()
+    val corpus = Io.rawCorpus(DataConsts.SIM_3_TRAIN_DOCS)
+    
+    println("BUILDING COLLAPSED SAMPLER")
+    val cg = new CollapsedGibbs(corpus, 15, 0.1, 0.1, corpus.length)
+    println("STATS:")
+    println("\tVocabulary:\t" + cg.W)
+    println("\tWords:\t\t" + cg.N)
+    println("\tDocuments:\t" + cg.D)
+    println()
+
+    println("RUNNING EXPERIMENT")
+    repeat(0, 3, cg)
+    //println("w: \"" + cg.w.deep.mkString("\" \"") + "\"")
+  }
+
+  def repeat (i: Int, n: Int, cg: CollapsedGibbs): Unit = {
+    if (i >= n) return
+    else {
+      cg.resampleTopic()
+      println("ASSIGNMENT " + i + " COMPLETE")
+      /*
+       println("w: \"" + cg.w.deep.mkString("\" \"") + "\"")
+       println("d: " + cg.d.deep.mkString(" "))
+       println("z: " + cg.z.deep.mkString(" "))
+       println("allAssignedZ: " + cg.allAssignedZ.deep.mkString(" "))
+       println("wAssignedZ: " + cg.wAssignedZ.deep.mkString(" "))
+       println("allAssignedZInD" + cg.allAssignedZInD.deep.mkString(" "))
+       cg.resampleTopic()
+       println("w: \"" + cg.w.deep.mkString("\" \"") + "\"")
+       println("d: " + cg.d.deep.mkString(" "))
+       println("z: " + cg.z.deep.mkString(" "))
+       println("allAssignedZ: " + cg.allAssignedZ.deep.mkString(" "))
+       println("wAssignedZ: " + cg.wAssignedZ.deep.mkString(" "))
+       println("allAssignedZInD" + cg.allAssignedZInD.deep.mkString(" "))
+       println()
+       */
+      repeat(i+1, n, cg)
+    }
+  }
+}
+
