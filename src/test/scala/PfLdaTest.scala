b11f6c2ddaafa1d4c1fdde13f7a926454c80bbf6
Alex Clemmer <clemmer.alexander@gmail.com>
2013-02-08 01:13:48 -0700

    Update very basic tests for PfLda

--- src/test/scala/PfLdaTest.scala
+++ src/test/scala/PfLdaTest.scala
@@ -10,10 +10,15 @@ import scala.collection.{ Set => Set }
 
 class PfLda extends FunSuite {
   test("Test ingestDoc for particle filter-based LDA") {
-    val pflda = new lda.PfLda(2, 0.1, 0.1, 2, 2)
+    val pflda = new lda.PfLda(2, 0.1, 0.1, 2, 20, 0.2)
     val corpus = Io.rawCorpus(DataConsts.SIM_3_TRAIN_DOCS)
     pflda.ingestDocs(corpus)
 
+    //println(pflda.Whitelist)
     println(pflda.vocabToId.size)
+    println("weights: " + pflda.pweights.deep)
+    println("cntWrdAssgTopicInCrps: " + pflda.cntWrdAssgTopicInCrps)
+    println("cntTopicAssgInCrps: " + pflda.cntTopicAssgInCrps.deep)
+    println(pflda.particles.deep)
   }
 }
