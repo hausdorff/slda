7b28da354758d3ee24c651b5c6cd9c276f9f3de5
Alex Clemmer <clemmer.alexander@gmail.com>
2013-02-08 01:12:35 -0700

    Move Stats object to Utils file

--- src/main/scala/gibbs/Gibbs.scala
+++ src/main/scala/gibbs/Gibbs.scala
@@ -8,6 +8,7 @@ import scala.annotation.tailrec
 import scala.util.{ Random => Random }
 import scala.collection.mutable.{ HashMap => HashMap }
 
+import lda.Stats
 import stream._
 import wrangle._
 
@@ -301,51 +302,3 @@ object GibbsUtil {
   }
 }
 
-object Stats {
-  val sampler = new Random()
-  
-  /** Returns normalized probability measure */
-  def normalize (arr: Array[Double]): Array[Double] = {
-    // WARNING SIDE EFFECTS ON `arr`
-    @tailrec
-    def loop (i: Int, sum: Double, acc: Array[Double]): Array[Double] =
-      if (i >= acc.length) {
-	acc(acc.length-1) = 1 // make sure last element is 1 and not eg 0.999
-	acc
-      }
-      else {
-	val curr = (acc(i) / sum) + acc(i-1)
-	acc(i) = curr
-	loop(i+1, sum, acc)
-      }
-    arr.length match {
-      case 0 => arr
-      case 1 => Array(1)
-      case _ => {
-	val sum = arr.reduceLeft(_+_)
-	arr(0) = arr(0) / sum
-	loop(1, sum, arr)
-      }
-    }
-  }
-  
-  /** Samples from simple categorical distribution; takes a normalized
-   probability measure and returns a randomly-sampled index */
-  def sampleCategorical (cdf: Array[Double]): Int = {
-    if (cdf.length == 0) throw new RuntimeException(
-      "can't sample form empty cdf")
-    else if (cdf.length == 1) return 0
-    
-    val r = sampler.nextDouble()
-    @tailrec
-    def loop (currIdx: Int): Int =
-      if (cdf(currIdx-1) <= r && r < cdf(currIdx)) currIdx
-      else loop(currIdx+1)
-    (cdf.length, r < cdf(0)) match {
-      case (0, _) => throw new RuntimeException("CDF has 0 elements!")
-      case (1, _) => 0
-      case (_, true) => 0
-      case (_, _) => loop(1)
-    }
-  }
-}
