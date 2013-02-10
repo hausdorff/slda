c03cfcd92d102b883585809c363acbf70346df8a
Alex Clemmer <clemmer.alexander@gmail.com>
2013-02-09 21:52:19 -0700

    Sketch out the particle filter LDA algorithm

--- src/main/scala/lda/PfLda.scala
+++ src/main/scala/lda/PfLda.scala
@@ -1,6 +1,7 @@
 package lda
 
 import scala.collection.mutable.{ HashMap => HashMap }
+import scala.util.{ Random => Random }
 
 import stream._
 import wrangle._
@@ -14,46 +15,43 @@ import wrangle._
  * @param rejuvSteps Number of steps to take before rejuvenating
  */
 class PfLda (val T: Int, val alpha: Double, val beta: Double,
-	     val smplSize: Int, val rejuvSteps: Int) {
+	     val smplSize: Int, val numParticles: Int, ess: Double) {
   val Whitelist = Text.stopWords(DataConsts.TNG_WHITELIST)
   var vocabToId = HashMap[String,Int]()
   var cntWrdAssgTopicInCrps = HashMap[(String,Int),Int]()
-  var cntTopicAssgInCrps = Array(T)(0)
-  var currWord = 0
+  var cntTopicAssgInCrps = Array.fill(T)(0)
+  var pweights = Array.fill(numParticles)(1.0/numParticles)
+  var particles = Array.fill(numParticles)(new Random().nextInt(T))
+  var vocabSize = 0
 
   /** Ingests set of documents, updating LDA run as we go */
   def ingestDocs (docs: Array[String]): Unit =
-    for (i <- 0 to docs.length-1) ingestDoc(docs(i))
+    docs.foreach{ doc => ingestDoc(doc) }
   
-  /*
-   we need:
-   # of times word w_j is assigned to topic z_j
-   # of times any word is assigned to topic z_j
-   # of times a word in doc d_i is assigned to topic z_j
-   # of wordsin document d_i
-   */
+  /** Ingest one document, update LDA as we go */
   def ingestDoc(doc: String): Unit = {
     // get all words in doc (remember to strip out whitelist
     val Words = Text.bow(doc, (str: String) => Whitelist(str))
-    
-    for (i <- 0 to Words.length-1) {
-      val w = Words(i)
-      // injest any new words into the word map
-      if (!(vocabToId contains Words(i))) {
-	vocabToId(w) = currWord
-	currWord += 1
-      }
-      // loop: every word, build and normalize the update distribution,
-      // sample, and place sampled assignment on word; then go on to the
-      // next word
-      
-
-      // add document to reservoir sample
-    }
+    var topicDistr = Array.fill(T)(0.0)
+    Words.foreach{ word => processWord(word, topicDistr) }
+  }
+  
+  private def processWord (word: String, topicDistr: Array[Double]): Unit = {
+    (0 to pweights.length-1).foreach { i => reweightParticle(i) }
+    // sample topic assignment for current word for each particle
+    (0 to particles.length-1).foreach { i => resampleParticle(i) }
+    // normalize weights
+    Stats.normalize(pweights)
+    // get inverse 2-norm of weights, check against ESS
+    if (Math.norm(pweights, 2) <= ess) rejuvenate()
   }
 
-  def unnormalizedPtUpdate (): Double = {
-    0.0
+  private def rejuvenate (): Unit = { }
+
+  private def reweightParticle (i: Int): Unit = {
+    // \omega_i^{(p)} = \omega_{i-1}^{(p)} P(w_i | z_{i-1}^{(p)}, w_{i-1})
   }
+
+  private def resampleParticle (i: Int): Unit = { }
 }
 
