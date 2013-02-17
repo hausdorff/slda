7613a830353ad88eeff811c918e508f70fba8e1f
Alex Clemmer <clemmer.alexander@gmail.com>
2013-02-17 01:38:09 -0700

    Add more tests to PfLda tests

--- src/test/scala/PfLdaTest.scala
+++ src/test/scala/PfLdaTest.scala
@@ -10,10 +10,28 @@ import scala.collection.{ Set => Set }
 
 class PfLda extends FunSuite {
   test("Test that PfLda is constructed correctly") {
-    var pflda = new lda.PfLda(3, 0.1, 0.1, 5, 5, 1)
+    val topics = 3
+    val alpha = 0.1
+    val beta = 0.1
+    val sampleSize = 5
+    val numParticles = 5
+    val ess = 1
+    var pflda = new lda.PfLda(topics, alpha, beta, sampleSize, numParticles,
+			      ess)
     val corpus = Array("the cow is short", "I can't find my glasses",
 		       "walnuts are delicious")
     pflda.ingestDocs(corpus)
+    
+    // Test globals roughly look ok
+    assert(pflda.currVocabSize == 5)
+    assert(pflda.rejuvSeq.size == 3)
+    assert(pflda.particles.size == 5)
+
+    var targetWeights = Array.fill(numParticles)(0.0)
+    (0 to numParticles-1).foreach { i =>
+      targetWeights(i) = pflda.particles(i).weight }
+    println(targetWeights.deep.mkString(" "))
+    assert(targetWeights.reduceLeft(_+_) == 1)
   }
   
   /*
