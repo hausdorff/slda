029aeb4540c2945f356a377ebc8194e75ec2cb86
Alex Clemmer <clemmer.alexander@gmail.com>
2013-02-17 01:08:31 -0700

    Begin to add tests for PfLda

--- src/test/scala/PfLdaTest.scala
+++ src/test/scala/PfLdaTest.scala
@@ -9,6 +9,14 @@ import scala.collection.{ Set => Set }
 
 
 class PfLda extends FunSuite {
+  test("Test that PfLda is constructed correctly") {
+    var pflda = new lda.PfLda(3, 0.1, 0.1, 5, 5, 1)
+    val corpus = Array("the cow is short", "I can't find my glasses",
+		       "walnuts are delicious")
+    pflda.ingestDocs(corpus)
+  }
+  
+  /*
   test("Test ingestDoc for particle filter-based LDA") {
     val pflda = new lda.PfLda(2, 0.1, 0.1, 2, 20, 0.2)
     val corpus = Io.rawCorpus(DataConsts.SIM_3_TRAIN_DOCS)
@@ -18,4 +26,5 @@ class PfLda extends FunSuite {
     println(pflda.vocabToId.size)
     println("weights: " + pflda.particles.deep)
   }
+  */
 }
