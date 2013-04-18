325facef8b00f2d11e23007445cab9144840be53
Alex Clemmer <clemmer.alexander@gmail.com>
2013-04-17 22:00:16 -0600

    Switch PfLdaTest from vanilla test to Steyvers Griffiths test
    By "Steyvers Griffiths test" we mean the one detailed in their paper
    "Probabilistic Topic Models."

--- src/test/scala/PfLdaTest.scala
+++ src/test/scala/PfLdaTest.scala
@@ -58,6 +58,67 @@ class PfLdaTests extends FunSuite {
     corpus
   }
 
+  /** results in array of tuples containing the mixture (element 1) and the
+   actual document (element 2) */
+  def generateSteyversGriffithsCorpus (): Array[(Array[Double],String)] = {
+    // (money, nature)
+    val documents = 16
+    var corpus = new Array[(Array[Double],String)](documents)
+
+    val mixture1 = Array[Double](0,1)
+    val mixture2 = Array[Double](0,1)
+    val mixture3 = Array[Double](0,1)
+    val mixture4 = Array[Double](0,1)
+    val mixture5 = Array[Double](0,1)
+    val mixture6 = Array[Double](0,1)
+    val mixture7 = Array[Double](0.625, 1)
+    val mixture8 = Array[Double](0.4375, 1)
+    val mixture9 = Array[Double](0.3125, 1)
+    val mixture10 = Array[Double](0.375, 1)
+    val mixture11 = Array[Double](0.6875, 1)
+    val mixture12 = Array[Double](0.6875, 1)
+    val mixture13 = Array[Double](0.875, 1)
+    val mixture14 = Array[Double](1, 1)
+    val mixture15 = Array[Double](1, 1)
+    val mixture16 = Array[Double](1, 1)
+
+    val doc1 = "bank bank bank bank money money money money money money loan loan loan loan loan loan"
+    val doc2 = "bank bank bank bank bank money money money money money money money loan loan loan loan"
+    val doc3 = "bank bank bank bank bank bank bank money money money money money loan loan loan loan"
+    val doc4 = "bank bank bank bank bank bank bank money money money money money money loan loan loan"
+    val doc5 = "bank bank bank bank bank bank bank money money loan loan loan loan loan loan loan"
+    val doc6 = "bank bank bank bank bank bank bank bank bank money money money loan loan loan loan"
+    val doc7 = "river bank bank bank bank money money money money money money loan loan loan loan loan"
+    val doc8 = "river stream stream bank bank bank bank bank bank money money money money loan loan loan"
+    val doc9 = "river stream stream stream bank bank bank bank bank bank money money money money loan loan"
+    val doc10 = "river river stream stream stream bank bank bank bank bank bank money loan loan loan loan"
+    val doc11 = "river river stream stream stream bank bank bank bank bank bank bank money money money loan"
+    val doc12 = "river river river stream stream stream stream stream stream bank bank bank bank bank bank money"
+    val doc13 = "river river river river river river stream stream stream bank bank bank bank bank bank loan"
+    val doc14 = "river river stream stream stream stream stream stream stream stream bank bank bank bank bank bank"
+    val doc15 = "river river river river stream stream stream stream stream stream stream bank bank bank bank bank"
+    val doc16 = "river river river river river stream stream stream stream stream stream stream bank bank bank bank"
+
+    corpus(0) = (mixture1, doc1)
+    corpus(1) = (mixture2, doc2)
+    corpus(2) = (mixture3, doc3)
+    corpus(3) = (mixture4, doc4)
+    corpus(4) = (mixture5, doc5)
+    corpus(5) = (mixture6, doc6)
+    corpus(6) = (mixture7, doc7)
+    corpus(7) = (mixture8, doc8)
+    corpus(8) = (mixture9, doc9)
+    corpus(9) = (mixture10, doc10)
+    corpus(10) = (mixture11, doc11)
+    corpus(11) = (mixture12, doc12)
+    corpus(12) = (mixture13, doc13)
+    corpus(13) = (mixture14, doc14)
+    corpus(14) = (mixture15, doc15)
+    corpus(15) = (mixture16, doc16)
+
+    corpus
+  }
+
   def standardPfLda (): PfLda = {
     val topics = 2
     val alpha = 0.1
@@ -74,7 +135,8 @@ class PfLdaTests extends FunSuite {
   }
 
   test("build test corpus") {
-    val corpus = generateCorpus()
+    //val corpus = generateCorpus()
+    val corpus = generateSteyversGriffithsCorpus()
     /*
      corpus.foreach{ item => println((item._1.deep.mkString(" "),
      item._2.deep.mkString(" "))) } */
