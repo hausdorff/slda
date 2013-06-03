edc2994c02fab8a2dbac74ef29e7274aee58ad33
Alex Clemmer <clemmer.alexander@gmail.com>
2013-06-03 13:06:27 -0400

    Add global member constant, plus additional comments

--- src/main/scala/Globals.scala
+++ src/main/scala/Globals.scala
@@ -1,5 +1,7 @@
 package globals
 
 object Constants {
-  val DidNotAddToSampler = -1 // used to signify 
+  val DidNotAddToSampler = -1 // signifies that we did not add the document to
+                              // the sampler in PfLda
+  val NoParent = -1 // indicates that particle does not have a parent
 }
