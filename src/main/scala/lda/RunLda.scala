02c289307cd8e719e33d7d024ac01e0794e44eeb
Alex Clemmer <clemmer.alexander@gmail.com>
2013-07-04 14:21:16 -0400

    Update parameters for running LDA

--- src/main/scala/lda/RunLda.scala
+++ src/main/scala/lda/RunLda.scala
@@ -7,9 +7,9 @@ object Sim3PfParams {
   val beta = 0.1
   val smplSize = 2980
   val numParticles = 100
-  val ess = 20
-  val rejuvBatchSize = 30
-  val rejuvMcmcSteps = 1000
+  val ess = 2
+  val rejuvBatchSize = 50
+  val rejuvMcmcSteps = 20
 }
 
 object RunLda {
